85311544d1407868a5af00f68adb2ef2e5c89543
Laurin van den Bergh <laurin.vandenbergh@gmail.com>
2022-02-02 22:16:45 +0100

    added text to "welcome demo" text

--- src/pages/home.jl
+++ src/pages/home.jl
@@ -28,6 +28,7 @@ readme = readme[start:end]
 if ARGS[1] == "demo"
     demo_welcome_text = """
     ## Welcome to this Dashboard Demo\n
+    __Note:__ Initial loading of the pages may take a while as the Julia programming language precompiles all plots. However, subsequent adaptions of the plots run relatively fast (considered the amount of data that needs to be handled and provided as interactive plots).\n
     This dashboard provides a usefull way to monitor and analyze ones personal finances to gain insigts into ones income streams and spending habits. This demo version is intended to demonstrate the functionality and versatility of the dashboard. If you would like some guidance on how to navigate the dashboard and how to interact with the plots, please see the sections below for some tips. We encurage the user to explore the dashboard and its features.\n\nWe display the full functionality of the dashboard on some artificial demo data. For users who are interested to try or use the dashboard with their own personal data (collected through the Moneyboard mobile app (not affiliated)), please clone/download the source code from this [GitHub repository](https://github.com/TwoDigitsOneNumber/PersonalFinanceDashboard). On the repository page we provide instructions on how to run the dashboard on your personal machine.\n\n
     """
     readme = demo_welcome_text * readme
